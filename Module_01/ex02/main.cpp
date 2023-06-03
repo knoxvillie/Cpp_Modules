215e6f09be67517e1f90d7bbf7c94427bc2d308b
Kelvin Faustino <kelvinfaustino@gmail.com>
2023-06-03 23:07:35 +0100

    ex02

---
+++ Module_01/ex02/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kfaustin <kfaustin@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/03 22:46:11 by kfaustin          #+#    #+#             */
+/*   Updated: 2023/06/03 23:07:12 by kfaustin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(void)
+{
+	std::string	str = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &str;
+	std::string	&stringREF = str;
+
+	std::cout << std::endl;
+	std::cout << "The memory address of the string variable: " << &str << std::endl;
+	std::cout << "The memory address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "The memory address of the string variable: " << &stringREF << std::endl;
+	std::cout << std::endl;
+	std::cout << "The value of the string variable: " << str << std::endl;
+	std::cout << "The value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "The value pointed to by stringREF: " << stringREF << std::endl << std::endl;
+	return (0);
+}
